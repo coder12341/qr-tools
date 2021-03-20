a51161e18792ed615ad54f5242d35669dd7d067b
Konstantinos <59742151+coder12341@users.noreply.github.com>
2021-03-20 14:20:54 +0200

    Second Commit

--- qr_generator.py
+++ qr_generator.py
@@ -1,100 +1,161 @@
 import qrcode
 from PIL import Image
-import argparse
+import segno
 
 
 class generator():
-    def generate_text(text, version, boxsize, border, fg, bg, logo, output):
-        qr = qrcode.QRCode(
-        version=version,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=boxsize,
-        border=border)
-        qr.add_data(text)
-        qr.make(fit=True)
-        img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
-        if logo != 'False':
-            logo_display = Image.open(logo)
-            logo_display.thumbnail((60, 60))
-            logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
-            img.paste(logo_display, logo_pos)
-        if output != 'False':
-            img.save(output)
+    def generate_text(text, version, boxsize, border, fg, bg, logo, output, logo_x, logo_y, micro, scale):
+        if micro==False:
+            qr = qrcode.QRCode(
+            version=version,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=boxsize,
+            border=border)
+            qr.add_data(text)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
+            if logo != 'False':
+                logo_display = Image.open(logo)
+                logo_display.thumbnail((60, 60))
+                logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
+                img.paste(logo_display, logo_pos)
+            if output != 'False':
+                img.save(output)
+            else:
+                img.save('generated_qr.png')
         else:
-            img.save('generated_qr.png')
+            qr = segno.make(text, micro=True)
+            if output != 'False':
+                qr.save(output+'.png', border=border, scale=scale)
+            else:
+                qr.save('generated_qr.png', border=border, scale=scale)
 
-    def generate_email(to, sub, body, version, boxsize, border, fg, bg, logo, output):
-        qr = qrcode.QRCode(
-        version=version,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=boxsize,
-        border=border)
-        qr.add_data('MATMSG:TO:%s;SUB:%s;BODY:%s;;'%(to, sub, body))
-        qr.make(fit=True)
-        img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
-        if logo != 'False':
-            logo_display = Image.open(logo)
-            logo_display.thumbnail((60, 60))
-            logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
-            img.paste(logo_display, logo_pos)
-        if output != 'False':
-            img.save(output)
+    def generate_email(to, sub, body, version, boxsize, border, fg, bg, logo, output, logo_x, logo_y, micro, scale):
+        if micro==False:
+            qr = qrcode.QRCode(
+            version=version,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=boxsize,
+            border=border)
+            qr.add_data('MATMSG:TO:%s;SUB:%s;BODY:%s;;'%(to, sub, body))
+            qr.make(fit=True)
+            img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
+            if logo != 'False':
+                logo_display = Image.open(logo)
+                logo_display.thumbnail((60, 60))
+                logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
+                img.paste(logo_display, logo_pos)
+            if output != 'False':
+                img.save(output)
+            else:
+                img.save('generated_qr.png')
         else:
-            img.save('generated_qr.png')
+            qr = segno.make('MATMSG:TO:%s;SUB:%s;BODY:%s;;'%(to, sub, body), micro=True)
+            if output != 'False':
+                qr.save(output+'.png', border=border, scale=scale)
+            else:
+                qr.save('generated_qr.png', border=border, scale=scale)
     
-    def generate_sms(to, message, version, boxsize, border, fg, bg, logo, output):
-        qr = qrcode.QRCode(
-        version=version,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=boxsize,
-        border=border)
-        qr.add_data('SMSTO:%s:%s'%(to, message))
-        qr.make(fit=True)
-        img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
-        if logo != 'False':
-            logo_display = Image.open(logo)
-            logo_display.thumbnail((60, 60))
-            logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
-            img.paste(logo_display, logo_pos)
-        if output !='False':
-            img.save(output)
+    def generate_sms(to, message, version, boxsize, border, fg, bg, logo, output, logo_x, logo_y, micro, scale):
+        if micro==False:
+            qr = qrcode.QRCode(
+            version=version,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=boxsize,
+            border=border)
+            qr.add_data('SMSTO:%s:%s'%(to, message))
+            qr.make(fit=True)
+            img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
+            if logo != 'False':
+                logo_display = Image.open(logo)
+                logo_display.thumbnail((60, 60))
+                logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
+                img.paste(logo_display, logo_pos)
+            if output !='False':
+                img.save(output)
+            else:
+                img.save('generated_qr.png')
         else:
-            img.save('generated_qr.png')
+            qr = segno.make('SMSTO:%s:%s'%(to, message), micro=True)
+            if output != 'False':
+                qr.save(output+'.png', border=border, scale=scale)
+            else:
+                qr.save('generated_qr.png', border=border, scale=scale)
 
-    def generate_wifi(ssid, passwd, encryption, hidden, version, boxsize, border, fg, bg, logo, output):
-        qr = qrcode.QRCode(
-        version=version,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=boxsize,
-        border=border)
-        qr.add_data("WIFI:T:%s;S:%s;P:%s;H:%s;"%(encryption, ssid, passwd, hidden))
-        qr.make(fit=True)
-        img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
-        if logo != 'False':
-            logo_display = Image.open(logo)
-            logo_display.thumbnail((60, 60))
-            logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
-            img.paste(logo_display, logo_pos)
-        if output != 'False':
-            img.save(output)
+    def generate_wifi(ssid, passwd, encryption, hidden, version, boxsize, border, fg, bg, logo, output, logo_x, logo_y, micro, scale):
+        if micro==False:
+            qr = qrcode.QRCode(
+            version=version,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=boxsize,
+            border=border)
+            qr.add_data("WIFI:T:%s;S:%s;P:%s;H:%s;"%(encryption, ssid, passwd, hidden))
+            qr.make(fit=True)
+            img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
+            if logo != 'False':
+                logo_display = Image.open(logo)
+                logo_display.thumbnail((logo_x, logo_y))
+                logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
+                img.paste(logo_display, logo_pos)
+            if output != 'False':
+                img.save(output)
+            else:
+                img.save('generated_qr.png')
         else:
-            img.save('generated_qr.png')
+            qr = segno.make("WIFI:T:%s;S:%s;P:%s;H:%s;"%(encryption, ssid, passwd, hidden), micro=True)
+            if output != 'False':
+                qr.save(output+'.png', border=border, scale=scale)
+            else:
+                qr.save('generated_qr.png', border=border, scale=scale)
     
-    def generate_contact_details(name, org, tel, email, cell, fax, street, city, region, postcode, country, url, version, boxsize, border, fg, bg, logo, output):
-        qr = qrcode.QRCode(
-        version=version,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=boxsize,
-        border=border)
-        qr.add_data('BEGIN:VCARD\nN:%s\nORG:%s\nEMAIL;TYPE=INTERNET:%s\nURL:%s\nTEL;TYPE=CELL:%s\nTEL:%s\nTEL;TYPE=FAX:%s\nADR:;;%s;%s;%s;%s;%s\nEND:VCARD'%(name, org, email, url, cell, tel, fax, street, city, region, postcode, country))
-        qr.make(fit=True)
-        img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
-        if logo != 'False':
-            logo_display = Image.open(logo)
-            logo_display.thumbnail((60, 60))
-            logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
-            img.paste(logo_display, logo_pos)
-        if output != 'False':
-            img.save(output)
+    def generate_contact_details(name, org, tel, email, cell, fax, street, city, region, postcode, country, url, version, boxsize, border, fg, bg, logo, output, logo_x, logo_y, micro, scale):
+        if micro==False:
+            qr = qrcode.QRCode(
+            version=version,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=boxsize,
+            border=border)
+            qr.add_data('BEGIN:VCARD\nN:%s\nORG:%s\nEMAIL;TYPE=INTERNET:%s\nURL:%s\nTEL;TYPE=CELL:%s\nTEL:%s\nTEL;TYPE=FAX:%s\nADR:;;%s;%s;%s;%s;%s\nEND:VCARD'%(name, org, email, url, cell, tel, fax, street, city, region, postcode, country))
+            qr.make(fit=True)
+            img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
+            if logo != 'False':
+                logo_display = Image.open(logo)
+                logo_display.thumbnail((60, 60))
+                logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
+                img.paste(logo_display, logo_pos)
+            if output != 'False':
+                img.save(output)
+            else:
+                img.save('generated_qr.png')
         else:
-            img.save('generated_qr.png')
+            qr = segno.make('BEGIN:VCARD\nN:%s\nORG:%s\nEMAIL;TYPE=INTERNET:%s\nURL:%s\nTEL;TYPE=CELL:%s\nTEL:%s\nTEL;TYPE=FAX:%s\nADR:;;%s;%s;%s;%s;%s\nEND:VCARD'%(name, org, email, url, cell, tel, fax, street, city, region, postcode, country), micro=True)
+            if output != 'False':
+                qr.save(output+'.png', border=border, scale=scale)
+            else:
+                qr.save('generated_qr.png', border=border, scale=scale)
+    
+    def genereate_event(title, location, start_time, end_time, version, boxsize, border, fg, bg , logo, output, logo_x, logo_y, micro, scale):
+        if micro==False:
+            qr = qrcode.QRCode(
+            version=version,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=boxsize,
+            border=border)
+            qr.add_data('BEGIN:VEVENT\nSUMMURY:%s\nLOCATION:%s\nDTSTART:%s\nDTEND:%s\nEND:VEVENT\n'%(title, location, start_time, end_time))
+            qr.make(fit=True)
+            img = qr.make_image(fill_color=fg, back_color=bg).convert('RGB')
+            if logo != 'False':
+                logo_display = Image.open(logo)
+                logo_display.thumbnail((60, 60))
+                logo_pos = ((img.size[0] - logo_display.size[0]) // 2, (img.size[1] - logo_display.size[1]) // 2)
+                img.paste(logo_display, logo_pos)
+            if output != 'False':
+                img.save(output)
+            else:
+                img.save('generated_qr.png')
+        else:
+            qr = segno.make('BEGIN:VEVENT\nSUMMARY:%s\nLOCATION:%s\nDTSTART:%s\nDTEND:%s\nEND:VEVENT\n'%(title, location, start_time, end_time), micro=True)
+            if output != 'False':
+                qr.save(output+'.png', border=border, scale=scale)
+            else:
+                qr.save('generated_qr.png', border=border, scale=scale)
--- qr_generator_gui.py
+++ qr_generator_gui.py
@@ -1,5 +1,54 @@
+'''
+                                                                      
+      .(@@@@@@@@@@@@@@@@@@@@@@@@/               .&@@@@@@@@@@@@@@@@@@@@@@@%,   
+    *@@@@@@@@@@@@@@@@@@@@@@@@@@@&               /@@@@@@@@@@@@@@@@@@@@@@@@@@@#   
+   #@@@@@&/....................                    ...................*#@@@@@&. 
+  .&@@@@&                                                               (@@@@@/ 
+  ,&@@@@&                                                               /@@@@@/ 
+  ,&@@@@&      #@@@@@@@@@@@@@@@@@@@@@@#   /@@@@@@@@@@@@@@@@@@@@@@&,     /@@@@@/ 
+  ,&@@@@&      %@@@%/************/#@@@%.  /@@@&/*************(@@@&,     /@@@@@/ 
+  ,&@@@@&      %@@@/              /@@@%.  /@@@#              ,&@@&,     /@@@@@/ 
+  ,&@@@@&      %@@@/    (####(.   /@@@%.  /@@@#    /#####.   ,&@@&,     /@@@@@/ 
+  ,&@@@@&      %@@@/    &@@@@&.   /@@@%.  /@@@#    #@@@@@,   ,&@@&,     /@@@@@/ 
+  ,&@@@@&      %@@@/    #&&&&#.   /@@@%.  /@@@#    (&&&&&,   ,&@@&,     /@@@@@/ 
+   *@@@%.      %@@@/              /@@@%.  /@@@#              ,&@@&,      (@@@(  
+               %@@@(,,,,,,,,,,,,,,(@@@%.  /@@@%,,,,,,,,,,,,,,/@@@&,             
+               %@@@@@@@@@@@@@@@@@@@@@@%.  /@@@@@@@@@@@@@@@@@@@@@@&,             
+                                                                                
+                .,,,,,,,,,,,,,,,,,,,,.                                          
+               %@@@@@@@@@@@@@@@@@@@@@@%.   .@@@@@(        ,%@@@@#               
+               %@@@(,,,,,,,,,,,,,,(@@@%.   ,@@@@@#        ,&@@@@%               
+   /@@@&,      %@@@/              /@@@%.   .(((((*        ./((((/        #@@@#. 
+  ,&@@@@&      %@@@/    %@@@@%.   /@@@%.           (@@@@@,              /@@@@@/ 
+  ,&@@@@&      %@@@/    &@@@@&.   /@@@%.           #@@@@@,              /@@@@@/ 
+  ,&@@@@&      %@@@/    (####(.   /@@@%.           *#####.              /@@@@@/ 
+  ,&@@@@&      %@@@/              /@@@%.   ,@@@@@#        ,&@@@@%       /@@@@@/ 
+  ,&@@@@&      %@@@%((((((((((((((%@@@%.   ,@@@@@#        ,&@@@@%       /@@@@@/ 
+  ,&@@@@&      #@@@@@@@@@@@@@@@@@@@@@@#    .(((((*        ./((((/       /@@@@@/ 
+  ,&@@@@&                                                               /@@@@@/ 
+  .&@@@@@                                                               (@@@@@/ 
+   #@@@@@@(...................                     ...................*&@@@@@&. 
+    ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@               (@@@@@@@@@@@@@@@@@@@@@@@@@@@#   
+      .(&@@@@@@@@@@@@@@@@@@@@@@@/               .&@@@@@@@@@@@@@@@@@@@@@@&#,                                                                        
+    
+    ░██████╗░██████╗░░█████╗░░█████╗░██████╗░███████╗
+    ██╔═══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝
+    ██║██╗██║██████╔╝██║░░╚═╝██║░░██║██║░░██║█████╗░░
+    ╚██████╔╝██╔══██╗██║░░██╗██║░░██║██║░░██║██╔══╝░░
+    ░╚═██╔═╝░██║░░██║╚█████╔╝╚█████╔╝██████╔╝███████╗
+    ░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░╚════╝░╚═════╝░╚══════╝
+
+    ░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+    ██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+    ██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+    ██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+    ╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+    ░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+'''
+
+
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QLineEdit, QPushButton, QTextEdit, QComboBox, QCheckBox, QSpinBox, QFileDialog, QMenu, QAction
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QTextEdit, QComboBox, QCheckBox, QSpinBox, QFileDialog, QMenu, QAction, QPlainTextEdit, QDateTimeEdit
 import sys
 from qr_generator import generator
 
@@ -7,11 +56,15 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('gui.ui', self)
-        self.setFixedSize(431, 357) 
+        self.setFixedSize(507, 357) 
         
         self.button1 = self.findChild(QPushButton, 'generate_1')
-        self.button1.clicked.connect(self.generate_text)
-        self.text_or_link = self.findChild(QLineEdit, "text_or_link")
+        self.button1.clicked.connect(self.generate_link)
+        self.link = self.findChild(QLineEdit, "link")
+
+        self.button7=self.findChild(QPushButton, 'generate_7')
+        self.button7.clicked.connect(self.generate_text)
+        self.text=self.findChild(QPlainTextEdit, 'text')
 
         self.button2 = self.findChild(QPushButton, 'generate_2')
         self.button2.clicked.connect(self.generate_email)
@@ -47,6 +100,16 @@ class Ui(QtWidgets.QMainWindow):
         self.country = self.findChild(QLineEdit, "country")
         self.url = self.findChild(QLineEdit, "url")
 
+        self.button8=self.findChild(QPushButton, 'generate_8')
+        self.button8.clicked.connect(self.generate_event)
+        self.event_title=self.findChild(QLineEdit, 'event_title')
+        self.event_location=self.findChild(QLineEdit, 'event_location')
+        self.st=self.findChild(QDateTimeEdit, 'start_time')
+        self.et=self.findChild(QDateTimeEdit, 'end_time')
+
+        self.scale=self.findChild(QSpinBox, 'scale')
+
+
         self.bg=self.findChild(QComboBox, 'bg')
         self.fg=self.findChild(QComboBox, 'fg')
         self.border=self.findChild(QSpinBox, 'border')
@@ -55,32 +118,35 @@ class Ui(QtWidgets.QMainWindow):
 
         self.logo=self.findChild(QPushButton, 'logo_2')
         self.logo_file='False'
+        self.logo_x=self.findChild(QSpinBox, 'logo_x')
+        self.logo_y=self.findChild(QSpinBox, 'logo_y')
         self.logo.clicked.connect(self.choose_logo)
         self.out_name=self.findChild(QLineEdit, 'output')
         self.filename='False'
+        self.micro=self.findChild(QCheckBox, 'microqr')
     
         self.show()
 
-    def generate_text(self): 
+    def generate_link(self): 
         if self.out_name.text():
             self.filename=self.out_name.text()
         else:
             self.filename='False'
-        generator.generate_text(self.text_or_link.text(), self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename)
+        generator.generate_text(self.link.text(), self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename, self.logo_x.value(), self.logo_y.value(), self.micro.isChecked(), self.scale.value())
 
     def generate_email(self):
         if self.out_name.text():
             self.filename=self.out_name.text()
         else:
             self.filename='False'
-        generator.generate_email(self.to_email.text(), self.subject.text(), self.email_message.toPlainText(), self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename)
+        generator.generate_email(self.to_email.text(), self.subject.text(), self.email_message.toPlainText(), self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename, self.logo_x.value(), self.logo_y.value(), self.micro.isChecked(), self.scale.value())
     
     def generate_sms(self):
         if self.out_name.text():
             self.filename=self.out_name.text()
         else:
             self.filename='False'
-        generator.generate_sms(self.to_phone.text(), self.sms_message.toPlainText(), self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename)
+        generator.generate_sms(self.to_phone.text(), self.sms_message.toPlainText(), self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename, self.logo_x.value(), self.logo_y.value(), self.micro.isChecked(), self.scale.value())
 
     def generate_wifi(self):
         if self.out_name.text():
@@ -92,26 +158,47 @@ class Ui(QtWidgets.QMainWindow):
             hidden_='true'
         else:
             hidden_='false'
-        generator.generate_wifi(self.ssid.text(), self.passwd.text(), self.encryption.currentText(), hidden_, self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename)
+        generator.generate_wifi(self.ssid.text(), self.passwd.text(), self.encryption.currentText(), hidden_, self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename, self.logo_x.value(), self.logo_y.value(), self.micro.isChecked(), self.scale.value())
     
     def generate_contact_details(self):
         if self.out_name.text():
             self.filename=self.out_name.text()
         else:
             self.filename='False'
-        generator.generate_contact_details(self.name.text(), self.org.text(), self.tel.text(), self.email.text(), self.cell.text(), self.fax.text(), self.street.text(), self.city.text(), self.region.text(), self.postcode.text(), self.country.text(), self.url.text, self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename)
+        generator.generate_contact_details(self.name.text(), self.org.text(), self.tel.text(), self.email.text(), self.cell.text(), self.fax.text(), self.street.text(), self.city.text(), self.region.text(), self.postcode.text(), self.country.text(), self.url.text, self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename, self.logo_x.value(), self.logo_y.value(), self.micro.isChecked(), self.scale.value())
+
+    def generate_text(self):
+        if self.out_name.text():
+            self.filename=self.out_name.text()
+        else:
+            self.filename='False'
+        generator.generate_text(self.text.toPlainText(), self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename, self.logo_x.value(), self.logo_y.value(), self.micro.isChecked(), self.scale.value())
+
+    def generate_event(self):
+        if self.out_name.text():
+            self.filename=self.out_name.text()
+        else:
+            self.filename='False'
+        
+        self.start_time=self.st.dateTime()
+        self.st_string = self.start_time.toString(self.st.displayFormat()).split(' ');self.stime=self.st_string[1].split(':');self.sdate=self.st_string[0].split('/')
+        self.start_datetime=self.sdate[2]+self.sdate[0]+self.sdate[1]+'T'+self.stime[0]+self.stime[1]+self.stime[2]
+        self.end_time=self.et.dateTime()
+        self.et_string = self.end_time.toString(self.st.displayFormat()).split(' ');self.etime=self.et_string[1].split(':');self.edate=self.et_string[0].split('/')
+        self.end_datetime=self.edate[2]+self.edate[0]+self.edate[1]+'T'+self.etime[0]+self.etime[1]+self.etime[2]
+        generator.genereate_event(self.event_title.text(), self.event_location.text(), self.start_datetime, self.end_datetime, self.qrversion.currentText(), self.boxsize.value(), self.border.value(), self.fg.currentText(), self.bg.currentText(), self.logo_file, self.filename, self.logo_x.value(), self.logo_y.value(), self.micro.isChecked(), self.scale.value())
 
     def choose_logo(self):
-        
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","PNG Files(*.png)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self,"Selct Logo Image", "","PNG Files(*.png)", options=options)
         if fileName:
             self.logo.setText(fileName)
             self.logo.setToolTip(fileName)
             self.logo_file=fileName
         else:
             self.logo_file='False'
+
     
 
 app = QtWidgets.QApplication(sys.argv)
